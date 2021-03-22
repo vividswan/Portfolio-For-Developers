062756b9f4b8e73891412509a20f5fe63747707a
vividswan <vividswan@naver.com>
2021-03-22 12:14:44 +0900

    feat: Swagger 설정 파일 생성 및 시큐리티에서 ignoring 설정

---
+++ back-end/src/main/java/com/portfolio/backend/config/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.portfolio.backend.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override // ignore check swagger resource
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**",
+                "/swagger-ui.html", "/webjars/**", "/swagger/**");
+
+    }
+
+}
