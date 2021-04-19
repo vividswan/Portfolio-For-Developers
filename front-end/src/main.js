4841e7b3f1647eedef72fd6b7b367d96a9ebc167
vividswan <vividswan@naver.com>
2021-04-19 14:00:12 +0900

    feat: 새로 고침 시 access token 조회로 로그인 여부 확인 기능 추가

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,12 +3,20 @@ import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 import vuetify from "./plugins/vuetify"
+import axios from "axios"
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = "http://localhost:5000/api"
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  beforeCreate() {
+    if (localStorage.getItem("access_token") !== null) {
+      this.$store.dispatch("getAccountInfo")
+    }
+  },
+  render: (h) => h(App)
 }).$mount("#app")
