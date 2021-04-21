7d61c3ff4b573296ecf55493dfc414a21c2303a8
vividswan <vividswan@naver.com>
2021-04-21 21:53:40 +0900

    feat: 회원 정보 조회 메소드 트랜잭션을 readOnly로 변경

--- back-end/src/main/java/com/portfolio/backend/account/AccountService.java
+++ back-end/src/main/java/com/portfolio/backend/account/AccountService.java
@@ -14,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.Collections;
 import java.util.UUID;
 
@@ -57,6 +57,7 @@ public class AccountService {
         return responseService.getSingleResponse(jwtTokenProvider.createToken(account.getEmail(), account.getRoles()));
     }
 
+    @Transactional(readOnly = true)
     public SingleResponse<AccountInfoResponse> getAccountInfo(String email) {
         Account account = accountRepository.findByEmail(email)
                 .orElseThrow(CustomUserNotFoundException::new);
