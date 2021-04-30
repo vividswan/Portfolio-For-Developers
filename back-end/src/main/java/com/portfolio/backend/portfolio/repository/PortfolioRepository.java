c86943b75ded8104462805515cfcb91390c2b437
vividswan <vividswan@naver.com>
2021-04-30 18:32:55 +0900

    fix: @EntityGraph 반영 내용 삭제

--- back-end/src/main/java/com/portfolio/backend/portfolio/repository/PortfolioRepository.java
+++ back-end/src/main/java/com/portfolio/backend/portfolio/repository/PortfolioRepository.java
@@ -3,9 +3,7 @@ package com.portfolio.backend.portfolio.repository;
 import com.portfolio.backend.portfolio.entitiy.Portfolio;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -13,8 +11,6 @@ public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
 
     Optional<Portfolio> findPortfolioByEmail(String email);
 
-    @EntityGraph(attributePaths = "projects")
-    @Query("select p from Portfolio p")
     Optional<Portfolio> findPortfolioByAccountNickname(String accountNickname);
 
     Optional<Portfolio> findPortfolioById(Long id);
