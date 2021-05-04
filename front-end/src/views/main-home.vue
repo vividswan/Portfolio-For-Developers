75968c5cbcf34351b388b35caaafc87fdf01d1ae
vividswan <vividswan@naver.com>
2021-05-04 14:19:32 +0900

    feat: 메인 페이지에 이미지 추가

--- front-end/src/views/main-home.vue
+++ front-end/src/views/main-home.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="home">
-    <h1>home</h1>
+    <v-col cols="12">
+      <v-img :src="require('../assets/discuss.svg')" contain />
+    </v-col>
+    <h6 class="text-center">
+      <div>
+        Icons made by
+        <a href="https://creativemarket.com/eucalyp" title="Eucalyp">Eucalyp</a>
+        from
+        <a href="https://www.flaticon.com/" title="Flaticon"
+          >www.flaticon.com</a
+        >
+      </div>
+    </h6>
   </div>
 </template>
 
